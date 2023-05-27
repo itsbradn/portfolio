32b2f48166a3f7a9f14340d4672d1b8fdc0bbe1a
braden <imbradn@protonmail.com>
2023-05-26 19:59:03 -0700

    Adding cloudflare SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   build: {
     assets: "_bradn"
-  }
+  },
+  adapter: cloudflare()
 });
